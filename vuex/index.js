be94568a8556f6b94526daede1b01be2722a2b00
吕子威 <18500905537@163.com>
2018-11-19 09:17:04 +0800

    超级简易版本的vuex

---
+++ vuex/index.js
@@ -0,0 +1,35 @@
+class Store {
+    constructor(options) {
+        let { state, mutations } = options;
+        this.state = state;
+        this.mutations = mutations;
+        resetStore(this); // 让store里的state变成响应式的
+    }
+
+    resetStore(store) {
+        store._vm = new Vue({
+            data: this.state
+        });
+    }
+
+    commit( type, payload ) {
+        this.mutations[type].call(this.state,payload)
+    }
+}
+
+function install(Vue) {
+    Vue.mixin( {
+        beforeCreate() {
+            if ( this.$options.store ) {
+                this.$store = this.$options.store
+            } else {
+                this.$store = this.$parent.store
+            }
+        }
+    })
+}
+
+export default {
+    Store,
+    install
+}
